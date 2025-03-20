1e4ba5e6203ed5b9782cb56ecd29373d0a04a687
carlos jesus <c.sipiran07@gmail.com>
2025-03-20 18:36:17 -0300

    Mejorar inicialización de propiedades en Products.cs
    Se han inicializado las propiedades `Title` y `Description` con `string.Empty` para evitar valores nulos. Además, la propiedad `Category` ahora se inicializa con una nueva instancia de `Category` en lugar de ser nula por defecto.

--- Models/Products.cs
+++ Models/Products.cs
@@ -6,14 +6,14 @@ namespace WebApiProducts.Models
     public class Products
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public string? Image { get; set; }
         public int Rate { get; set; }
         [Column(TypeName = "decimal(10,2)")]
         public double Price { get; set; }
         public int CategoryId { get; set; }
-        public virtual Category Category { get; set; }
+        public virtual Category Category { get; set; } = new Category();
 
         public Products()
         {
